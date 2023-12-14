dcd0e0cc311b3a3278de6612790057f616dbad52
ranzer97 <137756731+ranzer97@users.noreply.github.com>
2023-12-14 11:56:04 +0530

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,43 @@
+
+// document.getElementById('asd').addEventListener('submit',(e)=>{
+//     e.preventDefault()
+//     let fname=document.getElementById('fname').value;
+//     let lname=document.getElementById('lname').value;
+//     let user=document.getElementById('user').value;
+//     let email=document.getElementById('email').value;
+//     let pwd=document.getElementById('pwd').value;
+//     let cpwd=document.getElementById('cpwd').value;
+//   console.log(fname);
+
+//     if(pwd!=cpwd)
+//     {
+//         alert("password not match")
+//     }
+//     else{
+//         if(!(fname&&lname&&user&&email&&pwd&&cpwd))
+//         {
+//             alert("fields are empty")
+//         }
+//         else{
+//             alert("successfully loggedin")
+//         }
+//     }
+// })
+
+
+
+// // function asd()
+// // {
+// //     alert("oh nooo")
+// // }
+
+// document.getElementById('one').addEventListener('click',()=>{
+//     let a=document.getElementById('a').value
+//     let s=document.getElementById('s').value
+//     c=a+s
+
+//     // console.log(e.target.value);
+//     document.getElementById('f').innerHTML = c
+// })
+
+
---
+++ Main/sub/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+class Product_display(admin.ModelAdmin):
+    list_display=['name','Price','Brand','image']
+admin.site.register(Product,Product_display)
